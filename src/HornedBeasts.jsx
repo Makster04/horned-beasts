f6c63cadd3ee95e454a0d921f63c8bd9349add34
MAK TRNKA <maktrnka@comcast.net>
2024-02-29 09:45:33 -0800

    changes

--- src/HornedBeasts.jsx
+++ src/HornedBeasts.jsx
@@ -3,23 +3,27 @@ import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-
-
 const HornedBeasts = (props) => {
     const [likes, setLikes] = useState(0);
+
+    const handleLike = () => {
+        setLikes(likes + 1);
+    };
+
     return (
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={props.imageUrl} alt="horned beast image" />
-          <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-            <Card.Text>
-              {props.description}
-            </Card.Text>
-            <Button onClick={() => setLikes(likes + 1)} variant="primary">💖 Like</Button>
-          </Card.Body>
+            <Card.Img variant="top" src={props.imageUrl} alt="horned beast image" />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>
+                    {props.description}
+                </Card.Text>
+                <Button onClick={handleLike} variant="primary">💖 Like</Button>
+                <span style={{ marginLeft: '10px' }}>{likes} Likes</span>
+            </Card.Body>
         </Card>
-      );
-    }
+    );
+}
 
 export default HornedBeasts;
 

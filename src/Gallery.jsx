b219b0d2574ded007c2896125ddfdb27d602e0c0
MAK TRNKA <maktrnka@comcast.net>
2024-03-01 10:30:28 -0800

    Dropdown Added

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,27 +1,56 @@
+import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HornedBeasts from "./HornedBeasts";
 import data from './assets/data.json';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Row from 'react-bootstrap/Row';
 
-const Gallery = () => {
+// Dropdown Button
+const Gallery = (props) => {
+    console.log(props);
+
+    const [selectedHorns, setSelectedHorns] =useState(null);
+
+    const handleDropdownClick = (horns) => {
+        setSelectedHorns(horns);
+    }
+
+    const filteredData = selectedHorns ? data.filter(item => item.horns === selectedHorns) : data;
+
     return (
-        <div>
-            <h2>Gallery</h2>
+        <>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Filter by Number of Horns
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item onClick={() => handleDropdownClick(null)}>See All</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleDropdownClick(1)}>1 Horn</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleDropdownClick(2)}>2 Horns</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleDropdownClick(3)}>3 Horns</Dropdown.Item>
+                    <Dropdown.Item onClick={() => handleDropdownClick(100)}>100 Horns</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
             <div>
-            <Row xs="auto" md="auto" lg="auto">
-                {data.map(obj => {
-                return (
-                    <HornedBeasts 
-                    key={obj._id}
-                    title={obj.title} 
-                    description={obj.description} 
-                    imageUrl={obj.image_url} />  
-                )
-                    })}
-            </Row>
-            `</div>
-        </div>
-        
+                <h2>& where to find them!</h2>
+                <div>
+                <Row xs="auto" md="auto" lg="auto">
+                    {filteredData.map(obj => {
+                    return (
+                        <HornedBeasts 
+                        key={obj._id}
+                        title={obj.title} 
+                        description={obj.description} 
+                        imageUrl={obj.image_url}
+                        horns={obj.horns}
+                        onWin={props.onWin}
+                        />  
+                    )
+                        })}
+                </Row>
+                </div>
+            </div>
+        </>
     );
 };
 
